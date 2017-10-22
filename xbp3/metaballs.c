ae7bf1959df9a7a75481c9159c89b61ea64dd038
mar77i <mysatyre@gmail.com>
2017-10-22 14:12:15 +0200

    add an animated xor muncher to xbp3

--- xbp3/metaballs.c
+++ xbp3/metaballs.c
@@ -8,13 +8,13 @@
  */
 
 #include <limits.h>
-#include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
+#include "hsv.h"
 #include "xbp.h"
 
 #define BILLION (1000 * 1000 * 1000)
@@ -78,54 +78,6 @@ static inline int metaballs_dist(struct metaballs *m, int x, int y) {
 	if(dist > 255)
 		return 255;
 	return dist;
-}
-
-static inline void hsv_to_rgb(float result[],
-                              float h, float s, float v) {
-	float i, f, p, q, t;
-	if(s == 0) {
-		result[0] = v;
-		result[1] = v;
-		result[2] = v;
-		return;
-	}
-	i = floor(h * 6.0);
-	f = (h * 6.0) - i;
-	p = v * (1.0 - s);
-	q = v * (1.0 - s * f);
-	t = v * (1.0 - s * (1.0-f));
-	switch((int)i % 6) {
-	case 0:
-		result[0] = v;
-		result[1] = t;
-		result[2] = p;
-		return;
-	case 1:
-		result[0] = q;
-		result[1] = v;
-		result[2] = p;
-		return;
-	case 2:
-		result[0] = p;
-		result[1] = v;
-		result[2] = t;
-		return;
-	case 3:
-		result[0] = p;
-		result[1] = q;
-		result[2] = v;
-		return;
-	case 4:
-		result[0] = t;
-		result[1] = p;
-		result[2] = v;
-		return;
-	case 5:
-		result[0] = v;
-		result[1] = p;
-		result[2] = q;
-		return;
-	}
 }
 
 static inline float rsqrt(float n) {
---
+++ xbp3/xor.c
@@ -0,0 +1,45 @@
+
+// xor.c
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "hsv.h"
+#include "xbp.h"
+
+#define OFFSET_TYPE uint16_t
+#define OFFSET_MAX 1023
+
+int update(struct xbp *x, void *data) {
+	float rgb[4] = { .0 };
+	int px, py;
+	OFFSET_TYPE *offset = data;
+	uint8_t xor_value;
+	for(py = 0; py < x->attr.height; py++)
+		for(px = 0; px < x->attr.width; px++) {
+			xor_value = (px ^ py) + (*offset & 0xff);
+			hsv_to_rgb(rgb, (float)*offset / OFFSET_MAX, 1.0,
+			           (float)xor_value / 255);
+			((uint32_t*)x->data)[py * x->attr.width + px] = (
+				(int)(rgb[0] * 0xff) | ((int)(rgb[1] * 0xff) << 8) |
+				((int)(rgb[2] * 0xff) << 16) | (0xff << 24)
+			);
+		}
+	if(*offset >= OFFSET_MAX)
+		*offset = 0;
+	else
+		(*offset)++;
+	return 0;
+}
+
+int main(void) {
+	struct xbp x;
+	int ret = EXIT_FAILURE;
+	OFFSET_TYPE offset = 0;
+	if(xbp_init(&x, NULL) < 0)
+		return EXIT_FAILURE;
+	if(xbp_main(&x, update, NULL, &offset) == 0)
+		ret = EXIT_SUCCESS;
+	xbp_cleanup(&x);
+	return ret;
+}
