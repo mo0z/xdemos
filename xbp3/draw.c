286a56ab02dd2ec3c9639dd5bb70ca9339b5698a
mar77i <mar77i@protonmail.ch>
2018-02-04 11:16:20 +0100

    port another of my python hacks to xbp3

--- xbp3/draw.c
+++ xbp3/draw.c
@@ -9,11 +9,18 @@
 #include "hsv.h"
 #include "xbp.h"
 
+#define NUMDOTS 64
+#define WAIT_FRAMES 10
+
 #define SIGN(x) ((x) < 0 ? -1 : 1)
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 #define COORDCHECK(w, h, x, y) ( \
 	(x) >= 0 && (x) < (w) && (y) >= 0 && (y) < (h) \
 )
+
+struct draw {
+	size_t wait, step;
+};
 
 void draw_line(struct xbp *x, unsigned long color,
                int start_x, int start_y, int end_x, int end_y) {
@@ -42,18 +49,46 @@ use_y:
 	return;
 }
 
-int update(struct xbp *x) {
-	size_t i;
-	double rgb[3] = { 0.0, 0.0, 0.0 };
-	memset(xbp_ximage_data(x), 0,
-	       xbp_ximage_allo(x) * xbp_ximage_bytes_per_pixel(x));
-	for(i = 0; i < 1000; i++) {
-		hsv_to_rgb(rgb, (double)rand() / RAND_MAX, 1.0, .8);
-		draw_line(x, xbp_rgb8(x, rgb[0] * 255, rgb[1] * 255, rgb[2] * 255),
-		          rand() % XBP_WIDTH(x), rand() % XBP_HEIGHT(x),
-		          rand() % XBP_WIDTH(x), rand() % XBP_HEIGHT(x)
-		);
+static inline void get_pos(size_t w, size_t h, size_t step, size_t *x, size_t *y) {
+	step %= NUMDOTS * 4;
+	switch(step / NUMDOTS) {
+	case 0:
+		*x = w * step / NUMDOTS;
+		*y = 0;
+		break;
+	case 1:
+		*x = w - 1;
+		*y = h * (step - NUMDOTS) / NUMDOTS;
+		break;
+	case 2:
+		*x = w - w * (step - 2 * NUMDOTS) / NUMDOTS - 1;
+		*y = h - 1;
+		break;
+	case 3:
+		*x = 0;
+		*y = h - h * (step - 3 * NUMDOTS) / NUMDOTS - 1;
+		break;
 	}
+}
+
+int update(struct xbp *x) {
+	struct draw *d = xbp_get_data(x);
+	size_t w = XBP_WIDTH(x), h = XBP_HEIGHT(x), x1, x2, y1, y2;
+	double rgb[3] = { 0.0, 0.0, 0.0 };
+	if(++d->wait < WAIT_FRAMES)
+		return 0;
+	get_pos(w, h, d->step, &x1, &y1);
+	get_pos(w, h, d->step + NUMDOTS + 1, &x2, &y2);
+	hsv_to_rgb(rgb, (double)rand() / RAND_MAX, 1.0, .8);
+	draw_line(x, xbp_rgb8(x, rgb[0] * 255, rgb[1] * 255, rgb[2] * 255),
+	          x1, y1, x2, y2);
+	if(d->step > NUMDOTS + 1) {
+		x2 = x1;
+		y2 = y1;
+		get_pos(w, h, d->step - NUMDOTS - 1, &x1, &y1);
+		draw_line(x, BlackPixel(x->disp, x->scr), x1, y1, x2, y2);
+	}
+	d->step++;
 	return 0;
 }
 
@@ -71,9 +106,11 @@ int main(void) {
 		},
 	};
 	int ret = EXIT_FAILURE;
+	struct draw d = { 0, 0 };
 	srand(time(NULL));
 	if(xbp_init(&x, NULL) < 0)
 		return EXIT_FAILURE;
+	xbp_set_data(&x, &d);
 	if(xbp_main(&x) == 0)
 		ret = EXIT_SUCCESS;
 	xbp_cleanup(&x);
